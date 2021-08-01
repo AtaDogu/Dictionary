2d6d01a1cb2c92e3ab1cdda1e114d92cca4d7c2b
AtaDogu <88247572+AtaDogu@users.noreply.github.com>
2021-08-01 22:20:54 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var student = new Dictionary<int, string>();
+            student.Add(1, "Kamil Kemir");
+            student.Add(2, "Famil Femir");
+            student.Add(3, "Tamil Temir");
+            student.Add(4, "Pamil Pemir");
+            student.Add(5, "Zamil Zemir");
+            Console.Write("Öğrenci No Giriniz:");
+            int No = int.Parse(Console.ReadLine());
+            try
+            {
+                Console.WriteLine("Öğrenci Adı"+": "+student[No]);
+            }
+            catch
+            {
+                Console.WriteLine("Öğrenci Bulunamadı.");
+            }
+        }
+    }
+}
